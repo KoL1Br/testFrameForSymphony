c8649b12d37f3840ba6ae86d928a5e082ce1edfe
Pavlo Shkolnyi <pavlo.shkolnyi@globallogic.com>
2025-01-29 16:35:25 +0200

    refactor GoogleSearchResultsPage

--- TestProject/Pages/GoogleSearchResultsPage.cs
+++ TestProject/Pages/GoogleSearchResultsPage.cs
@@ -2,13 +2,12 @@ using OpenQA.Selenium;
 using System.Linq;
 
 namespace TestProject.Pages;
-public class GoogleSearchResultsPage : BasePage
+
+public class GoogleSearchResultsPage(IWebDriver driver) : BasePage(driver)
 {
     private readonly By _searchResults = By.XPath("//div[@id='search']//h3");
 
-    public GoogleSearchResultsPage(IWebDriver driver) : base(driver) { }
-
-    public bool AreResultsDisplayed() => FindElements(_searchResults).Any();
+    public bool AreResultsDisplayed() => FindElements(_searchResults).Count != 0;
 
     public bool FirstResultContains(string keyword)
     {
